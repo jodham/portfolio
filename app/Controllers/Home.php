7ac90d5e8c5fb67405bb8306cf361e873ea9dd1a
namaswaalice <alicenamaswa@gmail.com>
2024-06-03 09:02:44 +0300

    updated content

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Controllers;
-
+use CodeIgniter\Exceptions\PageNotFoundException;
 class Home extends BaseController
 {
-    public function index(): string
-    {
-        return view('welcome_message');
+
+    public function index(){
+        $page = 'index';
+        if (! is_file(APPPATH . 'Views/index/' . $page . '.php')) {
+            throw new PageNotFoundException($page);
+        }
+        $data['content'] = view('index/index', [], []); 
+        return view('index/base', $data);
     }
 }
---
+++ app/Views/index/base.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>iPortfolio Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?= base_url("assets/img/favicon.png")?>" rel="icon">
+  <link href="<?= base_url("assets/img/apple-touch-icon.png")?>" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?= base_url("assets/vendor/aos/aos.css")?>" rel="stylesheet">
+  <link href="<?= base_url("assets/vendor/bootstrap/css/bootstrap.min.css")?>" rel="stylesheet">
+  <link href="<?= base_url("assets/vendor/bootstrap-icons/bootstrap-icons.css")?>" rel="stylesheet">
+  <link href="<?= base_url("assets/vendor/boxicons/css/boxicons.min.css")?>" rel="stylesheet">
+  <link href="<?= base_url("assets/vendor/glightbox/css/glightbox.min.css")?>" rel="stylesheet">
+  <link href="<?= base_url("assets/vendor/swiper/swiper-bundle.min.css")?>" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?= base_url("assets/css/style.css")?>" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: iPortfolio
+  * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+<?php if(!empty($content))
+    {echo $content;}
+?>
+
+     <!-- Vendor JS Files -->
+  <script src="<?= base_url("assets/vendor/purecounter/purecounter_vanilla.js")?>"></script>
+  <script src="<?= base_url("assets/vendor/aos/aos.js")?>"></script>
+  <script src="<?= base_url("assets/vendor/bootstrap/js/bootstrap.bundle.min.js")?>"></script>
+  <script src="<?= base_url("assets/vendor/glightbox/js/glightbox.min.js")?>"></script>
+  <script src="<?= base_url("assets/vendor/isotope-layout/isotope.pkgd.min.js")?>"></script>
+  <script src="<?= base_url("assets/vendor/swiper/swiper-bundle.min.js")?>"></script>
+  <script src="<?= base_url("assets/vendor/typed.js/typed.umd.js")?>"></script>
+  <script src="<?= base_url("assets/vendor/waypoints/noframework.waypoints.js")?>"></script>
+  <script src="<?= base_url("assets/vendor/php-email-form/validate.js")?>"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?= base_url("assets/js/main.js")?>"></script>
+
+</body>
+
+</html>
